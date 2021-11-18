244a00302776af4470f6b1827debf88f93f73c2f
GuzGam360 <gioguzgam@gmail.com>
2021-11-18 00:10:58 -0600

    Updates

---
+++ precache-manifest.e8b618101ab1433d46434c2b4e00f0bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "167841d1aafca3aa09b562a3c0742d66",
+    "url": "/PsicologiaCarlos/index.html"
+  },
+  {
+    "revision": "c09649807abe7c1c6c9c",
+    "url": "/PsicologiaCarlos/static/css/2.7720f8b8.chunk.css"
+  },
+  {
+    "revision": "af46889669cf23377b95",
+    "url": "/PsicologiaCarlos/static/css/main.af79ed23.chunk.css"
+  },
+  {
+    "revision": "c09649807abe7c1c6c9c",
+    "url": "/PsicologiaCarlos/static/js/2.a7b11c84.chunk.js"
+  },
+  {
+    "revision": "af46889669cf23377b95",
+    "url": "/PsicologiaCarlos/static/js/main.a84335bc.chunk.js"
+  },
+  {
+    "revision": "8a2e88e119b75441aa1a",
+    "url": "/PsicologiaCarlos/static/js/runtime~main.37386275.js"
+  }
+]);
